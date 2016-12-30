22316bb3aba52f36d3a79fcb2ad8ef91b19118ab
Iuji Ujisato <iuji@vizir.com.br>
2016-12-30 16:33:06 -0200

    Split code and add route provider - Refactor code to add partials support. - Add angular routes module. - Set root path and prevents blank screen on not declared routes. - Set HTML5 mode to create friendlier routes.

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,16 @@
-angular.module('alurapic', ['alurapicDirective', 'ngAnimate']);
+angular
+  .module('alurapic', ['alurapicDirective', 'ngAnimate', 'ngRoute'])
+  .config(function($routeProvider, $locationProvider) {
+    $locationProvider.html5Mode(true);
+
+    $routeProvider.when('/photos', {
+      templateUrl: 'partials/photos/index.html',
+      controller: 'PhotosController'
+    });
+
+    $routeProvider.when('/photos/new', {
+      templateUrl: 'partials/photos/new.html',
+    });
+
+    $routeProvider.otherwise({ redirectTo: '/photos' });
+  });
